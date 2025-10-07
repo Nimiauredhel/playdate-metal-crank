83cb004066b09e62b76974dd43f259fe0167df8f
MickeyH@Yoga2 <hozmickey@gmail.com>
2025-10-07 16:01:00 +0300

    modified run/build script to not clutter the project root

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 mkdir -p build
 cd build
+mkdir -p bin
 cmake ..
-make
-PlaydateSimulator ../metal_crank.pdx
+cmake --build .
+mv ../metal_crank.pdx bin/metal_crank.pdx
+PlaydateSimulator bin/metal_crank.pdx
